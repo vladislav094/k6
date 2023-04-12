5f1b8f52b65d85c1111846237186c3e327433807
Vladislav.H <vkhrabrov94@gmail.com>
2023-04-12 13:00:50 +0300

    added script with multi-scenarios

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 import http from 'k6/http';
+import { check } from 'k6';
 
 export default function(){
     let url = 'https://httpbin.test.k6.io/post';
     let response = http.post(url, 'Hello World!');
 
-    console.log(response.json().data);
+    check(response, {
+        'Hello World included in response': (r) => r.body.includes('Hello Bonjour!')
+    });
 }
